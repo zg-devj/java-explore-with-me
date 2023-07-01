1c115822148e51594002e07c73767b03801f10be
Gleb Zakharov <webmaster-80@yandex.ru>
2023-07-01 22:30:02 +0300

    feat: add controllers without implementation

---
+++ main-service/src/main/java/ru/practicum/mainservice/admin/controllers/UserController.java
@@ -0,0 +1,50 @@
+package ru.practicum.mainservice.admin.controllers;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.mainservice.dtos.NewUserRequest;
+import ru.practicum.mainservice.dtos.UserDto;
+
+import javax.validation.constraints.PositiveOrZero;
+import java.util.List;
+
+/**
+ * API для работы с пользователями
+ */
+@Slf4j
+@RestController
+@RequestMapping("/admin/users")
+public class UserController {
+
+    // Получение информации о пользователях
+    @GetMapping
+    public List<UserDto> adminGetUsers(
+            @RequestParam(required = false) List<Long> ids,
+            @RequestParam(required = false, defaultValue = "0") @PositiveOrZero int from,
+            @RequestParam(required = false, defaultValue = "10") @PositiveOrZero int size
+    ) {
+        log.info("GET /admin/users?ids={}&from={}&size={} - Получение информации о пользователях.",
+                ids, from, size);
+        return null;
+    }
+
+    // Добавление нового пользователя
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public UserDto adminAddUser(
+            @RequestBody NewUserRequest newUserRequest
+    ) {
+        log.info("POST /admin/users - Добавление нового пользователя.");
+        return null;
+    }
+
+    // Удаление пользователя
+    @DeleteMapping("/{userId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void adminRemoveUser(
+            @PathVariable long userId
+    ) {
+        log.info("DELETE /admin/users/{} - Удаление пользователя.", userId);
+    }
+}
